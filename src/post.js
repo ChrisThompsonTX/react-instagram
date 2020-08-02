75f9c7f37cd102028c029ce64880571a5e288c39
Chris Thompson <chris.tx86@gmail.com>
2020-08-01 22:01:09 -0700

    add avatars

--- src/post.js
+++ src/post.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './post.css';
+import Avatar from "@material-ui/core/Avatar";
 
 function Post() {
     return (
         <div className="post">
-            <h3>Username</h3>    
+            <div className="post__header">
+                <Avatar 
+                    className="post__avatar"
+                    alt="username"
+                    src="/static/images/avatar/1.jpg"
+                />
+                <h3>Username</h3>    
+            </div>
             {/* header => avatar + username */}
             {/* <img className="post__image" src="https://www.freecodecamp.org/news/content/images/"/> */}
-            {/* image */}
-            <h4>Username: caption</h4>
-            {/* username + caption */}
+            <h4 className="post__text"><strong>username:</strong>caption</h4>
         </div>
     )
 }
